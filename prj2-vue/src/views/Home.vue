7d198b8db9fcf708befb2c65b1cc80a219e60d8c
Jeongdong <duwjdehd0713@gmail.com>
2021-09-06 10:06:33 +0900

    vue cli first commit

---
+++ prj2-vue/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <p>The quick brown fox ...</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
