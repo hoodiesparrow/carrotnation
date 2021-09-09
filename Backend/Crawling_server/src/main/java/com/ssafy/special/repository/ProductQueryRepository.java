98e2cd2e5bd83ced1655cd2013c1d1e43bd09d27
추헌국 <cngjsrnr@naver.com>
2021-09-09 13:16:40 +0900

    Fix 도메인 수정후 ProductQueryRepository 수정안해서 에러뜨던거 고침

--- Backend/Crawling_server/src/main/java/com/ssafy/special/repository/ProductQueryRepository.java
+++ Backend/Crawling_server/src/main/java/com/ssafy/special/repository/ProductQueryRepository.java
@@ -1,12 +1,9 @@
 package com.ssafy.special.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ssafy.special.domain.Product;
 import com.ssafy.special.domain.ProductQuery;
 
-public interface ProductQueryRepository extends JpaRepository<ProductQuery, Long>{
-	Optional<ProductQuery> findByProductIdAndMarket(Product productId, String market);
+public interface ProductQueryRepository extends JpaRepository<ProductQuery, String>{
+	
 }
