342b6e3f1f593ed4a10c86b3297950f8faa64d77
추헌국 <cngjsrnr@naver.com>
2021-09-17 14:45:25 +0900

    Feat 분석에 활용하기 위한 ProductSellList데이블 데이터가 쌓이는 ProductSellListStack 테이블 추가

---
+++ Backend/Crawling_server/src/main/java/com/ssafy/special/domain/ProductSellListStack.java
@@ -0,0 +1,48 @@
+package com.ssafy.special.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@ToString
+@IdClass(ProductSellListPK.class)
+public class ProductSellListStack {
+
+	@Id
+	private long id;//게시글 pid
+	@Id
+	private String market;//마켓종류
+		
+	@ManyToOne
+	@JoinColumn(name="productId")
+	private Product productId;//품명
+	
+	private String title;//제목
+	
+	@Column(length = 1024)
+	private String content;//내용
+	
+	private long price;//가격
+	
+	private LocalDateTime createDate;//작성일자
+	
+	private String link;//게시글 상세보기 링크
+	
+	private String img;//이미지 링크
+	
+	private String location;//지역
+}
