4e79db904052fcd0263d410da381a6d6942f9874
추헌국 <cngjsrnr@naver.com>
2021-09-24 18:40:04 +0900

    Fix ProductSellList프론트로 뿌려주는 api에 pid 조건 추가, responseDTO 새로 만들어서 뿌려줌, 리턴할때 전체 페이지수 같이 리턴해줌

--- Backend/Crawling_server/src/main/java/com/ssafy/special/controller/APIController.java
+++ Backend/Crawling_server/src/main/java/com/ssafy/special/controller/APIController.java
@@ -1,6 +1,8 @@
 package com.ssafy.special.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.special.domain.ProductSellList;
+import com.ssafy.special.dto.ProductSellListResponseDTO;
 import com.ssafy.special.service.ProductSellListInfoService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,15 +25,24 @@ public class APIController {
 
 	//ProductSellList 뿌려줌(최신사이클만)
 	@GetMapping("/productselllist")
-	public ResponseEntity<List<ProductSellList>> getProductSellList(@RequestParam(defaultValue = "0") int page) {
-			List<ProductSellList> productSellLists = productSellListInfoService.getProductSellLists(page);
+	public ResponseEntity<Map<String, Object>> getProductSellList(@RequestParam(defaultValue = "0") int page, @RequestParam long pid) {
+			List<ProductSellListResponseDTO> productSellLists = productSellListInfoService.getProductSellLists(page, pid);
+			Long count = productSellListInfoService.getProductSellListCount(pid);
+			
+			Map<String, Object> ret = new HashMap<String, Object>();
 			
 			if(productSellLists==null) {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 			}else if(productSellLists.size()==0) {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+				ret.put("msg", "페이지, 제품번호를 다시 확인해 주세요");
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ret);
 			}
+			long totalpage= count/20;
+			if(count%20>0)
+				totalpage+=1;
+			ret.put("totalpage", totalpage);
+			ret.put("list", productSellLists);			
 			
-			return ResponseEntity.status(HttpStatus.OK).body(productSellLists);
+			return ResponseEntity.status(HttpStatus.OK).body(ret);
 	}
 }
---
+++ Backend/Crawling_server/src/main/java/com/ssafy/special/dto/ProductSellListResponseDTO.java
@@ -0,0 +1,34 @@
+package com.ssafy.special.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductSellListResponseDTO {
+	private long id;//게시글 pid
+	private String market;//마켓종류
+		
+	private long productId;//품명
+	
+	private String title;//제목
+	
+	private String content;//내용
+	
+	private long price;//가격
+	
+	private LocalDateTime createDate;//작성일자
+	
+	private String link;//게시글 상세보기 링크
+	
+	private String img;//이미지 링크
+	
+	private String location;//지역
+
+	private Long cycle;//크롤링 주기 파악을 위해 만듬
+}
