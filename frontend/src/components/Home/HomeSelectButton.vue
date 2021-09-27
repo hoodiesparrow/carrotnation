7725e1e08d53c5f07cb361214fa41907185173df
Jeongdong <duwjdehd0713@gmail.com>
2021-09-27 11:18:25 +0900

    Fix not merged file update

--- frontend/src/components/Home/HomeSelectButton.vue
+++ frontend/src/components/Home/HomeSelectButton.vue
@@ -9,7 +9,7 @@
           leave-active-class="transition transform duration-200"
           leave-from-class="translate-x-0"
           leave-to-class="-translate-x-full"
-          @after-leave="tranFlag2=true"
+          @after-leave="tranFlag2 = true"
         >
           <div v-if="tranFlag1">
             <span class="text-lg">[{{ treeTag }}]</span>
@@ -29,7 +29,16 @@
                       : '',
                     checked ? 'bg-blue-900 bg-opacity-75 text-white ' : 'bg-white ',
                   ]"
-                  class="relative flex px-5 py-4 rounded-lg shadow-md cursor-pointer focus:outline-none"
+                  class="
+                    relative
+                    flex
+                    px-5
+                    py-4
+                    rounded-lg
+                    shadow-md
+                    cursor-pointer
+                    focus:outline-none
+                  "
                   @click="onClickT1(idx)"
                 >
                   <div class="flex items-center justify-between w-full">
@@ -52,13 +61,7 @@
                     </div>
                     <div v-show="checked" class="flex-shrink-0 text-white">
                       <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none">
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="12"
-                          fill="#fff"
-                          fill-opacity="0.2"
-                        />
+                        <circle cx="12" cy="12" r="12" fill="#fff" fill-opacity="0.2" />
                         <path
                           d="M7 13l3 3 7-7"
                           stroke="#fff"
@@ -81,7 +84,7 @@
           leave-active-class="transition transform duration-200"
           leave-from-class="translate-x-0"
           leave-to-class="-translate-x-full"
-          @after-leave="tranFlag1=true"
+          @after-leave="tranFlag1 = true"
         >
           <div v-if="tranFlag2">
             <span class="text-lg">[{{ treeTag }}]</span>
@@ -101,7 +104,16 @@
                       : '',
                     checked ? 'bg-blue-900 bg-opacity-75 text-white ' : 'bg-white ',
                   ]"
-                  class="relative flex px-5 py-4 rounded-lg shadow-md cursor-pointer focus:outline-none"
+                  class="
+                    relative
+                    flex
+                    px-5
+                    py-4
+                    rounded-lg
+                    shadow-md
+                    cursor-pointer
+                    focus:outline-none
+                  "
                   @click="onClickT2(idx)"
                 >
                   <div class="flex items-center justify-between w-full">
@@ -124,13 +136,7 @@
                     </div>
                     <div v-show="checked" class="flex-shrink-0 text-white">
                       <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none">
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="12"
-                          fill="#fff"
-                          fill-opacity="0.2"
-                        />
+                        <circle cx="12" cy="12" r="12" fill="#fff" fill-opacity="0.2" />
                         <path
                           d="M7 13l3 3 7-7"
                           stroke="#fff"
@@ -152,37 +158,37 @@
 </template>
 
 <script>
-import get from 'lodash/get'
-import { computed, reactive, ref } from 'vue'
-import { useStore } from 'vuex'
+import get from "lodash/get";
+import { computed, reactive, ref } from "vue";
+import { useStore } from "vuex";
 import {
   // TransitionRoot,
   RadioGroup,
   RadioGroupLabel,
   RadioGroupDescription,
   RadioGroupOption,
-} from '@headlessui/vue'
+} from "@headlessui/vue";
 
 const plans = [
   {
-    name: 'Startup',
-    ram: '12GB',
-    cpus: '6 CPUs',
-    disk: '160 GB SSD disk',
+    name: "Startup",
+    ram: "12GB",
+    cpus: "6 CPUs",
+    disk: "160 GB SSD disk",
   },
   {
-    name: 'Business',
-    ram: '16GB',
-    cpus: '8 CPUs',
-    disk: '512 GB SSD disk',
+    name: "Business",
+    ram: "16GB",
+    cpus: "8 CPUs",
+    disk: "512 GB SSD disk",
   },
   {
-    name: 'Enterprise',
-    ram: '32GB',
-    cpus: '12 CPUs',
-    disk: '1024 GB SSD disk',
+    name: "Enterprise",
+    ram: "32GB",
+    cpus: "12 CPUs",
+    disk: "1024 GB SSD disk",
   },
-]
+];
 
 export default {
   components: {
@@ -195,30 +201,28 @@ export default {
   },
 
   setup() {
-    const store = useStore()
-    const selected = ref(plans[0])
-    const currentTran = ref(1)
-    const tranFlag1 = ref(true)
-    const tranFlag2 = ref(false)
+    const store = useStore();
+    const selected = ref(plans[0]);
+    const currentTran = ref(1);
+    const tranFlag1 = ref(true);
+    const tranFlag2 = ref(false);
 
-    const tree1Depth = ref(['category'])
-    const tree1 = ref(get(store.getters['getCategoryData'], tree1Depth.value))
-    const tree1Content = computed(() => 
-      Object.keys(tree1.value).slice(1)
-    )
+    const tree1Depth = ref(["category"]);
+    const tree1 = ref(get(store.getters["getCategoryData"], tree1Depth.value));
+    const tree1Content = computed(() => Object.keys(tree1.value).slice(1));
     // const tree1Content = ref(Object.keys(tree1.value).slice(1))
-    console.log(tree1)
+    console.log(tree1);
 
-    const tree2Depth = ref(['category'])
-    const tree2 = ref([])
-    const tree2Content = computed(() => 
-      Object.keys(tree2.value).slice(1)
-    )
+    const tree2Depth = ref(["category"]);
+    const tree2 = ref([]);
+    const tree2Content = computed(() => Object.keys(tree2.value).slice(1));
 
     const treeTag = computed(() => {
       // 얘도 분리해야 도중에 안바뀜..but 빨리 지나가서 보이지는 않네 ㅋㅋ
-      return tranFlag1.value ? tree1.value[Object.keys(tree1.value)[0]] : tree2.value[Object.keys(tree2.value)[0]]
-    })
+      return tranFlag1.value
+        ? tree1.value[Object.keys(tree1.value)[0]]
+        : tree2.value[Object.keys(tree2.value)[0]];
+    });
     // 내일 할것
     // 1. 데이터 집어넣기
     // 2. depth 눌러서 뒤로가기 하면 3번째 그룹을 만드는 것이 아니라
@@ -226,41 +230,40 @@ export default {
     // 그러면 원복하는 로직은 어떻게 하지... timeout? 아니면 바로 수정해 버리면 어떻게 되는지 확인
     // 일단은 당장 떠오르는 방법은 없다. 아니면 @click에 그 객체를 수정하는 코드를 달아보리기
     // 버튼부터 끝내놓고 api 관련 작업을 하던 뭘 하던 해보자
-    // depth 버튼 만드는것도 문제네... 아마도 .splice 사용하면 될 것 같긴 해 ㅇㅇ 
+    // depth 버튼 만드는것도 문제네... 아마도 .splice 사용하면 될 것 같긴 해 ㅇㅇ
     // v-for에서 key를 임의idx로 잡아놓고 @click에서 인자로 넘기면 될듯
 
     const onClickT1 = (idx) => {
-      
       // treeDepth수정
-      tree1Depth.value.push(tree1Content.value[idx])
-      tree2Depth.value.push(tree1Content.value[idx])
+      tree1Depth.value.push(tree1Content.value[idx]);
+      tree2Depth.value.push(tree1Content.value[idx]);
       // tag가 있으면?
-      tree2.value = get(store.getters['getCategoryData'], tree2Depth.value)
-      console.log(tree2.value)
-      if (Object.keys(tree2.value).indexOf('tag') === -1) {
-        console.log('end of the tree, router push required')
+      tree2.value = get(store.getters["getCategoryData"], tree2Depth.value);
+      console.log(tree2.value);
+      if (Object.keys(tree2.value).indexOf("tag") === -1) {
+        console.log("end of the tree, router push required");
       } else {
-        console.log('button animation called')
-        tranFlag1.value = false
+        console.log("button animation called");
+        tranFlag1.value = false;
       }
-    }
+    };
     const onClickT2 = (idx) => {
       // treeDepth수정
-      tree1Depth.value.push(tree2Content.value[idx])
-      tree2Depth.value.push(tree2Content.value[idx])
+      tree1Depth.value.push(tree2Content.value[idx]);
+      tree2Depth.value.push(tree2Content.value[idx]);
       // tag가 있으면?
-      tree1.value = get(store.getters['getCategoryData'], tree1Depth.value)
-      console.log(tree1.value)
-      if (Object.keys(tree1.value).indexOf('tag') === -1) {
-        console.log('end of the tree, router push required')
+      tree1.value = get(store.getters["getCategoryData"], tree1Depth.value);
+      console.log(tree1.value);
+      if (Object.keys(tree1.value).indexOf("tag") === -1) {
+        console.log("end of the tree, router push required");
       } else {
-        console.log('button animation called')
-        tranFlag2.value = false
+        console.log("button animation called");
+        tranFlag2.value = false;
       }
-    }
+    };
 
-    return { 
-      selected, 
+    return {
+      selected,
       plans,
       tranFlag1,
       tranFlag2,
@@ -274,9 +277,9 @@ export default {
       tree2Depth,
       tree2Content,
       treeTag,
-    }
+    };
   },
-}
+};
 </script>
 
 // console.log(get(store.getters['getCategoryData'], deviceCategory.value))
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,19 +2,32 @@ module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    container: {
-      center: true,
-    },
-    maxWidth: {
-      '750px': '750px',
-    },
-    width: {
-      '180px': '180px',
-      '750px': '750px',
-    },
-    minWidth: {
-      '2000px': '2000px',
+    extend: {
+      container: {
+        center: true,
+      },
+      maxWidth: {
+        '750px': '750px',
+      },
+      minWidth: {
+        '2000px': '2000px',
+      },
+      width: {
+        '180px': '180px',
+        '200px': '200px',
+        '230px': '230px',
+        '250px': '250px',
+        '750px': '750px',
+      },
+      maxHeight: {
+        '250px': '250px',
+        '500px': '500px',
+      },
+      colors: {
+        'background': '#ffffff',
+        'mattBlack': '#161b23',
+        'pointPurple': '#e3d8ff',
+      },
     },
   },
   variants: {
