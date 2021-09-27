87724288815e2c0b0f39cd71b93319726f7422c5
추헌국 <cngjsrnr@naver.com>
2021-09-27 17:44:11 +0900

    Fix cors에러 안뜨게 수정

--- Backend/Crawling_server/src/main/java/com/ssafy/special/controller/APIController.java
+++ Backend/Crawling_server/src/main/java/com/ssafy/special/controller/APIController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*" )
 public class APIController {
 
 	private final ProductSellListInfoService productSellListInfoService;
