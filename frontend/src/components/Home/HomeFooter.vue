cea42554674a386606168ddde6f4b5ff3a3720f7
dongyun <hoodiesparrow@gmail.com>
2021-10-04 14:34:18 +0900

    Fix 아이폰12 기종 데이터 추가

---
+++ frontend/src/components/Home/HomeFooter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="max-w-750px w-full absolute bottom-0 overflow-hidden">
+    <transition
+      enter-active-class="transition transform duration-300"
+      enter-from-class="translate-y-full"
+      enter-to-class="translate-y-0"
+      leave-active-class="transition transform duration-300"
+      leave-from-class="translate-y-0"
+      leave-to-class="translate-y-full"
+      @after-leave="showDiv = true"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" v-if="showButton" class="max-w-750px w-full flex justify-center my-5" @click="showButton = false">
+        <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.033 16.01c.564-1.789 1.632-3.932 1.821-4.474.273-.787-.211-1.136-1.74.209l-.34-.64c1.744-1.897 5.335-2.326 4.113.613-.763 1.835-1.309 3.074-1.621 4.03-.455 1.393.694.828 1.819-.211.153.25.203.331.356.619-2.498 2.378-5.271 2.588-4.408-.146zm4.742-8.169c-.532.453-1.32.443-1.761-.022-.441-.465-.367-1.208.164-1.661.532-.453 1.32-.442 1.761.022.439.466.367 1.209-.164 1.661z"/>
+      </svg>
+    </transition>
+    <transition
+      enter-active-class="transition transform duration-300"
+      enter-from-class="translate-y-full"
+      enter-to-class="translate-y-0"
+      leave-active-class="transition transform duration-300"
+      leave-from-class="translate-y-0"
+      leave-to-class="translate-y-full"
+      @after-leave="showButton = true"
+    >
+      <div v-if="showDiv" class="bg-gray-900 bg-opacity-10 backdrop-blur" @click="showDiv = false">
+        당근나라 번개시세 개발자 정보
+        <br>
+        이용약관 | 모든 상품들에 대해서 당근나라 번개시세는 거래 당사자가 아니며 거래에 관여하지 않고 어떠한 의무와 책무도 부담하지 않습니다.
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeFooter',
+  setup() {
+    const showButton = ref(true)
+    const showDiv = ref(false)
+
+    return { 
+      showButton,
+      showDiv,
+     }
+  }
+}
+</script>
--- frontend/src/views/Test3.vue
+++ frontend/src/views/Test3.vue
@@ -1,64 +1,136 @@
+<template>
+  <div class="w-full max-w-md px-2 py-16 sm:px-0">
+    <TabGroup>
+      <TabList class="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
+        <Tab
+          v-for="category in Object.keys(categories)"
+          as="template"
+          :key="category"
+          v-slot="{ selected }"
+        >
+          <button
+            :class="[
+              'w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg',
+              'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+              selected
+                ? 'bg-white shadow'
+                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+            ]"
+          >
+            {{ category }}
+          </button>
+        </Tab>
+      </TabList>
+
+      <TabPanels class="mt-2">
+        <TabPanel
+          v-for="(posts, idx) in Object.values(categories)"
+          :key="idx"
+          :class="[
+            'bg-white rounded-xl p-3',
+            'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+          ]"
+        >
+          <ul>
+            <li
+              v-for="post in posts"
+              :key="post.id"
+              class="relative p-3 rounded-md hover:bg-coolGray-100"
+            >
+              <h3 class="text-sm font-medium leading-5">
+                {{ post.title }}
+              </h3>
+
+              <ul
+                class="flex mt-1 space-x-1 text-xs font-normal leading-4 text-coolGray-500"
+              >
+                <li>{{ post.date }}</li>
+                <li>&middot;</li>
+                <li>{{ post.commentCount }} comments</li>
+                <li>&middot;</li>
+                <li>{{ post.shareCount }} shares</li>
+              </ul>
+
+              <a
+                href="#"
+                :class="[
+                  'absolute inset-0 rounded-md',
+                  'focus:z-10 focus:outline-none focus:ring-2 ring-blue-400',
+                ]"
+              />
+            </li>
+          </ul>
+        </TabPanel>
+      </TabPanels>
+    </TabGroup>
+  </div>
+</template>
+
 <script>
-import { defineComponent } from 'vue'
-import { Line } from 'vue3-chart-v2'
+import { ref } from 'vue'
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 
-export default defineComponent({
-  name: 'MonthlyChart',
-  extends: Line,
-  mounted () {
-    // Overwriting base render method with actual data.
-    const canvas = this.$refs.canvas
-
-    const gradient = canvas
-      .getContext('2d')
-      .createLinearGradient(0, 0, 0, 450)
-
-    gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
-    gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
-    gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
-
-    const gradient2 = canvas
-      .getContext('2d')
-      .createLinearGradient(0, 0, 0, 450)
-
-    gradient2.addColorStop(0, "rgba(0, 231, 255, 0.9)");
-    gradient2.addColorStop(0.5, "rgba(0, 231, 255, 0.25)")
-    gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
-
-
-    this.renderChart({
-      labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July"
-      ],
-      datasets: [
+export default {
+  components: {
+    TabGroup,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+  },
+  setup() {
+    let categories = ref({
+      Recent: [
         {
-          label: "시세",
-          borderColor: "#FC2525",
-          pointBackgroundColor: "white",
-          borderWidth: 1,
-          pointBorderColor: "white",
-          tension: 0.3,
-          backgroundColor: gradient,
-          data: [40, 39, 10, 40, 39, 80, 40],
+          id: 1,
+          title: 'Does drinking coffee make you smarter?',
+          date: '5h ago',
+          commentCount: 5,
+          shareCount: 2,
         },
         {
-          label: "예상 가격",
-          borderColor: "#05CBE1",
-          pointBackgroundColor: "white",
-          pointBorderColor: "white",
-          borderWidth: 1,
-          tension: 0.3,
-          backgroundColor: gradient2,
-          data: [60, 55, 32, 10, 2, 12, 53]
-        }
-      ]
+          id: 2,
+          title: "So you've bought coffee... now what?",
+          date: '2h ago',
+          commentCount: 3,
+          shareCount: 2,
+        },
+      ],
+      Popular: [
+        {
+          id: 1,
+          title: 'Is tech making coffee better or worse?',
+          date: 'Jan 7',
+          commentCount: 29,
+          shareCount: 16,
+        },
+        {
+          id: 2,
+          title: 'The most innovative things happening in coffee',
+          date: 'Mar 19',
+          commentCount: 24,
+          shareCount: 12,
+        },
+      ],
+      Trending: [
+        {
+          id: 1,
+          title: 'Ask Me Anything: 10 answers to your questions about coffee',
+          date: '2d ago',
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: '4d ago',
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
     })
-  }
-})
+
+    return { categories }
+  },
+}
 </script>
