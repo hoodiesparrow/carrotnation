8218776ab99df746c9110e57ea142d54758fd912
dongyun <hoodiesparrow@gmail.com>
2021-10-07 01:00:47 +0900

    Fix delete test page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,9 +2,6 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const Prod = () => import(/* webpackChunkName: "prod", */ '@/views/Prod.vue');
 const Home = () => import(/* webpackChunkName: "home", */ '@/views/Home.vue');
-const Test = () => import(/* webpackChunkName: "test", */ '@/views/Test.vue');
-const Test2 = () => import(/* webpackChunkName: "test", */ '@/views/Test2.vue');
-const Test3 = () => import(/* webpackChunkName: "test", */ '@/views/Test3.vue');
 const Detail = () => import(/* webpackChunkName: "detail", */ '@/views/Detail.vue');
 const Quote = () => import(/* webpackChunkName: "quote" */ '@/views/Quote.vue');
 
@@ -14,30 +11,10 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: Home,
   },
-  // {
-  //   path: '/chart',
-  //   name: 'Chart',
-  //   component: Chart,
-  // },
   {
     path: '/prod',
     name: 'Prod',
     component: Prod,
-  },
-  {
-    path: '/test',
-    name: 'Test',
-    component: Test,
-  },
-  {
-    path: '/test2',
-    name: 'Test2',
-    component: Test2,
-  },
-  {
-    path: '/test3',
-    name: 'Test3',
-    component: Test3,
   },
   {
     path: '/detail',
--- frontend/src/views/Test2.vue
+++
@@ -1,7 +0,0 @@
-<script>
-import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
-
-export default {
-
-}
-</script>
